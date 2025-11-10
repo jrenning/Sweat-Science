265cf4cab1b9652d8b64731c449ef73cb2a3f2c6
Jack Renning <jackalexrenning@gmail.com>
2025-11-10 17:43:42 -0600

    try again again.

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -49,7 +49,7 @@
 		> -->
 		<div></div>
 	{:else}
-		<SignIn signInPage="/auth/signin">
+		<SignIn>
 			<button slot="submitButton" class="rounded-md bg-primary-500 shadow-md px-2 py-1"
 				>Sign In with Google</button
 			>
