5ffb89f428f7deec30d2a94986f09b6691539ee0
Jack Renning <jackalexrenning@gmail.com>
2025-11-10 12:33:42 -0600

    Remove index route.

--- src/routes/index/schemas.ts
+++
@@ -1,7 +0,0 @@
-import type { insertExerciseSchema } from '$lib/db/schema';
-
-export type insertExerciseSchema = typeof insertExerciseSchema;
-
-import type { insertEquipmentSchema } from '$lib/db/schema';
-
-export type insertEquipmentSchema = typeof insertEquipmentSchema;
