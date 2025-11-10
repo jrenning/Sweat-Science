807f96f88182034d801f5ac609affbdb09e895e8
Jack Renning <jackalexrenning@gmail.com>
2025-11-10 12:39:50 -0600

    Add auth secret.

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,6 +1,6 @@
 import { SvelteKitAuth } from '@auth/sveltekit';
 import Google from '@auth/core/providers/google';
-import { GOOGLE_ID, GOOGLE_SECRET } from '$env/static/private';
+import { AUTH_SECRET, GOOGLE_ID, GOOGLE_SECRET } from '$env/static/private';
 import { DrizzleAdapter } from '@auth/drizzle-adapter';
 import { db } from '$lib/db/db';
 import type { Handle } from '@sveltejs/kit';
@@ -8,6 +8,8 @@ import type { Handle } from '@sveltejs/kit';
 // export const trpcHandle: Handle = createTRPCHandle({ router, createContext });
 export const {handle, signIn, signOut} = SvelteKitAuth(async (event) => {
 	const authOptions = {
+		secret: AUTH_SECRET,
+    	trustHost: true,
 		adapter: DrizzleAdapter(db),
 		providers: [Google({ clientId: GOOGLE_ID, clientSecret: GOOGLE_SECRET })],
 		callbacks: {
